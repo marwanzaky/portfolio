fff0af7f59c3d6fdd8d17107d6cee9ea8a097f3a
Marwan Zaky <64248203+marwanzaky@users.noreply.github.com>
2021-10-01 00:17:23 +0100

    Restyling the headers, and more enhancements to the blog style

--- venders/js/blog.js
+++ venders/js/blog.js
@@ -1,17 +1,23 @@
 var posts = [
     {
         title: 'Enemy Vision',
-        des: ' Enemy Vision is a script collection to add vision cones, raycast detection, and enemy patrolling behaviors to your game.',
-        img: 'venders/img/enemy-vision.png'
+        des: 'Enemy Vision is a script collection to add vision cones, raycast detection, and enemy patrolling behaviors to your game.',
+        date: 'September 30, 2021',
+        img: 'venders/img/enemy-vision.png',
+        link: 'https://github.com/marwanzaky/Enemy-Vision'
     },
     {
         title: 'Audio Manager Tool',
         des: 'Unity Audio Manager is a tool to manage your Audios',
-        img: 'venders/img/audio-manager-tool.png'
+        date: 'September 30, 2021',
+        img: 'venders/img/audio-manager-tool.png',
+        link: 'https://github.com/marwanzaky/Unity-Audio-Manager'
     }, {
-        title: 'Unity Screenshot Tool',
-        des: 'Unity Screenshot Tool for Capturing the current frame.',
-        img: 'venders/img/screenshot-tool.png'
+        title: 'Unity Screenshot Tool for Capturing the current frame',
+        des: 'Unity Audio Manager is a tool to manage your Audios',
+        date: 'September 30, 2021',
+        img: 'venders/img/screenshot-tool.png',
+        link: 'https://github.com/marwanzaky/Unity-Screenshot-Tool'
     },
 ];
 
@@ -19,12 +25,15 @@ var blogs = '';
 
 posts.forEach(el => {
     blogs += `
-    <div class="blog">
-        <h2>${el.title}</h2>
+    <article class="blog">
+        <h4>${el.date}</h4>
+        <div class="blog-cover">
+            <a href="${el.link}" target=”_blank”><img src="${el.img}" alt="${el.img}"></a>
+        </div>
+        <h3>${el.title}</h3>
         <p>${el.des}</p>
-        <img src="${el.img}" alt="" style="height: 200px;">
-    </div`
+        <a href="#" class="blog-readmore">Read More »</a>
+    </article>`
 });
 
 document.querySelector('.blogs').innerHTML = blogs;
-console.log(blogs);
