aa9a7ec9802ab70e9c1d169766995168b6b2d946
Marwan Zaky <marouaneezzaky@gmail.com>
2021-12-26 16:53:43 +0100

    Smooth scroll to sections

--- venders/js/navigator.js
+++ venders/js/navigator.js
@@ -1,4 +1,24 @@
 function jump(h) {
-    var el = document.querySelector('.' + h);
-    window.scrollTo(0, el.offsetTop);
+    var element = document.querySelector('.' + h);
+    var elementY = element.getBoundingClientRect().top;
+    var windowY = window.pageYOffset;
+
+    function step(timestamp) {
+        windowY = MathX.lerp(windowY, elementY, .1);
+        window.scrollTo(0, windowY);
+
+        if (Math.abs(windowY - elementY) > 1)
+            window.requestAnimationFrame(step);
+        else window.scrollTo(0, elementY);
+    }
+
+    window.requestAnimationFrame(step);
 }
+
+var MathX = {
+    lerp: function (a, b, t) {
+        t = t < 0 ? 0 : t;
+        t = t > 1 ? 1 : t;
+        return a + (b - a) * t;
+    }
+};
