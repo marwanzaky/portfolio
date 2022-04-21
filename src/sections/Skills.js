18c7f5aeac763706f19b93fb1a2598e36f27f6ab
Marwan Zaky <marouaneezzaky@gmail.com>
2022-04-21 10:34:11 +0000

    Responsive skills section on all devices

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -13,7 +13,7 @@ function Skill(props) {
         </a>
     }
 
-    return <div className='skill'>
+    return <div className='col-sm-4 p-4 skill'>
         <div>{props.isIcon ? <IconURL /> : <URL />}</div>
         <div className='skills-title'>{props.title}</div>
         <p>{props.description}</p>
@@ -27,15 +27,17 @@ function Skills() {
             <h2>Why Me?</h2>
         </div>
 
-        <div className="container skills-box">
-            <Skill isIcon={true} class='skill-icon' href='https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/' name='code-slash-outline' title='C#' description='
+        <div className='container'>
+            <div className="row skills-box">
+                <Skill isIcon={true} class='skill-icon' href='https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/' name='code-slash-outline' title='C#' description='
             An advanced C# programmer making flexible, expandable and self-explanatory code, and custom tools for productivity.' />
 
-            <Skill isIcon={false} class='skill-icon fab fa-unity' href='https://unity3d.com/' name='fab fa-unity' title='Unity' description='
+                <Skill isIcon={false} class='skill-icon fab fa-unity' href='https://unity3d.com/' name='fab fa-unity' title='Unity' description='
             +7 years of experience making professional iOS/Android games from scratch. especially hyper-casual games.' />
 
-            <Skill isIcon={true} class='skill-icon' href='https://en.wikipedia.org/wiki/Gameplay' name='game-controller-outline' title='Gameplay' description='
+                <Skill isIcon={true} class='skill-icon' href='https://en.wikipedia.org/wiki/Gameplay' name='game-controller-outline' title='Gameplay' description='
             A smooth gameplay experience is an essential part of every successful game, we are making sure to deliver delightful UX.' />
+            </div>
         </div>
     </section>
 }
