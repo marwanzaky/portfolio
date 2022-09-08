71dd34038e758c7d77fc29eb85c385314d928e8f
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-08 20:12:21 +0100

    Alt attribute to footer icons

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -6,7 +6,7 @@ function List(props) {
     return (
         <li className='footer-contacts-li'>
             <a className='footer-contacts-li-a' href={props.url} target='_blank' rel='noopener noreferrer'>
-                <img className='footer-contacts-li-a-icon' src={props.src} />
+                <img className='footer-contacts-li-a-icon' src={props.src} alt={props.alt} />
             </a>
         </li>
     )
@@ -21,8 +21,8 @@ function Footer() {
                 </div>
 
                 <div className='w-[50%] flex flex-row justify-end footer-contacts'>
-                    <List url='https://github.com/marwanzaky' src={upworkIcon} />
-                    <List url='https://www.upwork.com/freelancers/~016c440becd0ed6d83' src={githubIcon} />
+                    <List src={upworkIcon} alt='Upwork icon' url='https://github.com/marwanzaky' />
+                    <List src={githubIcon} alt='Github icon' url='https://www.upwork.com/freelancers/~016c440becd0ed6d83' />
                 </div>
             </div>
         </footer>
