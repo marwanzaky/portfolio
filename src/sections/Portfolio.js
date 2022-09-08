08cf99a17ae3a85561362921645c1e4ec266000d
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-08 19:55:17 +0100

    Update Footer.js, and Portfolio.js

--- src/sections/Portfolio.js
+++ src/sections/Portfolio.js
@@ -17,7 +17,7 @@ function Project(props) {
     return (
         <div className="portfolio-project" style={props.style} >
             <a href={props.url} target="_blank">
-                <img className='portfolio-project-img' src={props.src} alt={props.text} />
+                <img className='portfolio-project-img' src={props.src} alt={props.text} rel='noopener noreferrer' />
                 <div className="portfolio-project-background"></div>
                 <span className="portfolio-project-text">{props.text}</span>
             </a>
