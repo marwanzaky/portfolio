3164f5db50847ee2b03983302dbd0261eb774305
Marwan Zaky <marouaneezzaky@gmail.com>
2022-09-15 00:29:46 +0100

    Update styles.scss

--- src/sections/Timelines.js
+++ src/sections/Timelines.js
@@ -16,30 +16,38 @@ function Timelines() {
             <div className='xl:container xl:mx-auto section-timelines-container'>
                 <h2>Timelines</h2>
 
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-[1.25rem]'>
                     <Timeline duration='May 2021 &#8212; Present' title='Web Developer Developer' company='Upwork' >
-                        &#x2022;  Translate requirements into complicated but clean and efficient code.<br />
-                        &#x2022;  Develop schedules and determine milestones.<br />
-                        &#x2022;  Polish the game, maintain code, fix bugs, and iron out occurring problems.
+                        <ul>
+                            <li>&#x2022;  Translate requirements into complicated but clean and efficient code.</li>
+                            <li>&#x2022;  Develop schedules and determine milestones.</li>
+                            <li>&#x2022;  Polish the game, maintain code, fix bugs, and iron out occurring problems.</li>
+                        </ul>
                     </Timeline>
 
                     <Timeline duration='Jun 2020 &#8212; Oct 2021' title='Unity Game Developer' company='Aiwa Holding' >
-                        &#x2022;  Building a simulator iOS/Android game from the ground up to finish a game.<br />
-                        &#x2022;  Implementation of features using flexible and salable programming patterns.<br />
-                        &#x2022;  Integration to user Authentication and Database.
+                        <ul>
+                            <li>&#x2022;  Building a simulator iOS/Android game from the ground up to finish a game.</li>
+                            <li>&#x2022;  Implementation of features using flexible and salable programming patterns.</li>
+                            <li>&#x2022;  Integration to user Authentication and Database.</li>
+                        </ul>
                     </Timeline>
 
                     <Timeline duration='Jul 2020 &#8212; Jun 2021' title='Unity Game Developer' company='Applift'>
-                        &#x2022;  Responsible for developing hyper-casual games prototypes.<br />
-                        &#x2022;  Produce prototypes of gameplay ideas and features.<br />
-                        &#x2022;  Core gameplay systems and UI/UX.
+                        <ul>
+                            <li>&#x2022;  Responsible for developing hyper-casual games prototypes.</li>
+                            <li>&#x2022;  Produce prototypes of gameplay ideas and features.</li>
+                            <li>&#x2022;  Core gameplay systems and UI/UX.</li>
+                        </ul>
                     </Timeline>
 
                     <Timeline duration='Mar 2020 &#8212; Dec 2020' title='Unity Game Developer' company='Leela Games'  >
-                        Responsible for different aspects.<br />
-                        &#x2022;  Mesh generation gameplay mechanics.<br />
-                        &#x2022;  Core gameplay systems and UI/UX.<br />
-                        &#x2022;  Level design.
+                        <p>Responsible for different aspects.</p>
+                        <ul>
+                            <li>&#x2022;  Mesh generation gameplay mechanics.</li>
+                            <li>&#x2022;  Core gameplay systems and UI/UX.</li>
+                            <li>&#x2022;  Level design.</li>
+                        </ul>
                     </Timeline>
                 </div>
                 {/* </div> */}
