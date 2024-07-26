9c275d96b5d74ba3c34734f25a8c8ce49a151eea
Marwan Zaky <marouaneezzaky@gmail.com>
2024-07-26 18:53:08 +0100

    init commit for v5.0 development

--- components/timelines/timeline/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Timeline(props) {
-    return <div className='section-timelines-timeline'>
-        <h4 className='section-timelines-timeline-title'>{props.title} <span className='section-timelines-timeline-title-company'>- {props.company}</span></h4>
-        <div className='section-timelines-timeline-duration'>{props.duration}</div>
-        <div className='section-timelines-timeline-des'>{props.children}</div>
-    </div>
-}
